e5ffeed11d30b01b12b5cf069292fd40b7b996ae
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-09-20 16:16:59 +0530

    NotificationCenter - Example 2 Added
    - Sending messages form one VC to another VC

---
+++ NotificationCenter/NotificationCenter/Controllers/ViewController1.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController1.swift
+//  NotificationCenter
+//
+//  Created by mallikarjun on 20/09/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
